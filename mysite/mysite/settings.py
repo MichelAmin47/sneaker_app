72f1062ab3ee65ad7a406239206bf2219757bb8e
Michel Lalmohamed <michellalmohamed@valori.nl>
2022-10-28 21:16:27 +0200

    database models created

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'sneaker_app.apps.SneakerAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
