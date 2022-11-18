1829092c30fbaec134da5c5cf734fc86726a8066
Michel Lalmohamed <michellalmohamed@valori.nl>
2022-11-18 17:49:51 +0100

    example poll site done

--- mysite/sneaker_app/admin.py
+++ mysite/sneaker_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question
+from .models import Shoe
+
+admin.site.register(Question)
+admin.site.register(Shoe)
+
--- mysite/sneaker_app/urls.py
+++ mysite/sneaker_app/urls.py
@@ -3,5 +3,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /sneaker_app/
     path('', views.index, name='index'),
+    # ex: /sneaker_app/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /sneaker_app/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /sneaker_app/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+
+    # ex: /sneaker_app/5/
+    path('shoe_model/<int:shoe_id>/', views.shoe_model, name='shoe model'),
 ]
